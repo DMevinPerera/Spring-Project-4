18856ee57a6283881e69fac1f91f054337fff378
MevinPerera <87105802+DMevinPerera@users.noreply.github.com>
2024-11-21 20:03:40 +0530

    first commit

---
+++ src/main/java/com/Spring_Project_4/Spring_Project_4/SpringProject4Application.java
@@ -0,0 +1,13 @@
+package com.Spring_Project_4.Spring_Project_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring_Project_4/Spring_Project_4/SpringProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Project_4.Spring_Project_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
